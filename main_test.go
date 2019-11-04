f53a8a1e56d9795a5d413b38ad71e310e4f0398a
peter walugembe <peterwade153@gmail.com>
2019-11-04 20:35:24 +0300

    add update item func (#3)

--- main_test.go
+++ main_test.go
@@ -24,3 +24,30 @@ func TestCreateItem(t *testing.T){
 	}
 
 }
+
+func TestEditItem(t *testing.T){
+	// creating item to edit
+	var dummydata = []byte (`{"id":5, "title":"title", "description":"description"}`)
+	req, err := http.NewRequest("POST", "/items", bytes.NewBuffer(dummydata))
+	if err != nil{
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	res := httptest.NewRecorder()
+	handler := http.HandlerFunc(controllers.CreateItem)
+	handler.ServeHTTP(res, req)
+
+	// editting the item
+	var editdata = []byte (`{"id":5, "title":"title", "description":"description"}`)
+	editreq, editerr := http.NewRequest("PUT", "/items", bytes.NewBuffer(editdata))
+	if editerr != nil{
+		t.Fatal(err)
+	}
+	editreq.Header.Set("Content-Type", "application/json")
+	editres := httptest.NewRecorder()
+	edithandler := http.HandlerFunc(controllers.EditItem)
+	edithandler.ServeHTTP(editres, editreq)
+	if status := editres.Code; status != http.StatusOK{
+		t.Errorf("wrong status code: got %v instead of %v", status, http.StatusOK)
+	}
+}
